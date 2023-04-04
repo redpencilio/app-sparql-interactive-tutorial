f1d5cd3194f8e6a9c67dfc3aa5e9465e139643ab
Laurent Deketelaere <laurent.deketelaere@redpencil.io>
2023-04-04 16:31:21 +0200

    Fix(sparql): Add `application/n-triples` in sparql `define_accept_types`

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -3,7 +3,7 @@ defmodule Dispatcher do
   define_accept_types [
     html: [ "text/html", "application/xhtml+html" ],
     json: [ "application/json", "application/vnd.api+json" ],
-    sparql: [ "application/sparql-results+json" ]
+    sparql: [ "application/sparql-results+json", "application/n-triples" ]
   ]
 
   @any %{}
